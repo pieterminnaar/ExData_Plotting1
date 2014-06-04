9326fd06e7b873f4fa1ea66ecf88d953b982e2b6
Pieter Minnaar <pieter.minnaar@gmail.com>
2014-06-04 21:36:54 +0200

    plot2 is ok...

--- plot2.R
+++ plot2.R
@@ -5,20 +5,20 @@ library(data.table)
 # Unzip file in data directory in the same folder as this R file
 
 suppressWarnings(DT <- fread("data/household_power_consumption.txt")) 
+  
 
 DT[, Date := as.Date(Date, "%d/%m/%Y")]
-
 
 filterDays <- c(as.Date("2007-02-01", "%Y-%m-%d"), as.Date("2007-02-02", "%Y-%m-%d"))
 
 twoDays <- (DT$Date == filterDays[1]) | (DT$Date == filterDays[2]) 
 
 plot.data <- DT[twoDays,]
-
 plot.data$Global_active_power <- as.numeric(plot.data$Global_active_power)
+plot.times <- strptime(paste(as.character(plot.data$Date), plot.data$Time), 
+                             "%Y-%m-%d %H:%M:%S")
 
-png("plot1.png", bg="white", width=480, height=480)
-hist(plot.data$Global_active_power, breaks = seq(0, 7.5, by = 0.5), col="red", 
-     xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power")
+png("plot2.png", bg="white", width=480, height=480)
+plot(plot.times, plot.data$Global_active_power, type="l" , 
+    ylab = "Global Active Power (kilowatts)", xlab = "")     
 dev.off()
