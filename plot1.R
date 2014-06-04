d53b49f79b3370c395cd9bf4949ed459b6102b6b
Pieter Minnaar <pieter.minnaar@gmail.com>
2014-06-04 20:50:06 +0200

    plot 1...

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
 library(data.table)
 
-suppressWarnings(DT <- fread("../household_power_consumption.txt"))
+# Download the file:
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Unzip file in data directory in the same folder as this R file
 
+suppressWarnings(DT <- fread("data/household_power_consumption.txt")) 
 strtotime <- function(a_string) {
   vals <- unlist(strsplit(a_string, ":"))
   nums <- as.numeric(vals)
@@ -10,4 +13,18 @@ strtotime <- function(a_string) {
   
 
 DT[, Date := as.Date(Date, "%d/%m/%Y")]
-DT[, Time := strtotime(Time)]
+suppressWarnings(DT[, Time := strtotime(Time)])
+
+filterDays <- c(as.Date("2007-02-01", "%Y-%m-%d"), as.Date("2007-02-02", "%Y-%m-%d"))
+
+twoDays <- (DT$Date == filterDays[1]) | (DT$Date == filterDays[2]) 
+
+plot.data <- DT[twoDays,]
+
+plot.data$Global_active_power <- as.numeric(plot.data$Global_active_power)
+
+png("plot1.png", bg="white", width=480, height=480)
+hist(plot.data$Global_active_power, breaks = seq(0, 7.5, by = 0.5), col="red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+dev.off()
